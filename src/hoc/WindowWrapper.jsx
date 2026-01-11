db58d690aaa5873b9efa31f2241e51bf2ec01436
cerebralstoic <vineet.rawat.ddun62019@gmail.com>
2026-01-11 23:38:30 +0530

    implemented hoc for  managing window and implemented terminal window

---
+++ src/hoc/WindowWrapper.jsx
@@ -0,0 +1,62 @@
+import React, { useLayoutEffect } from 'react'
+import useWindowStore from '#store/window';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { Draggable } from 'gsap/Draggable';
+gsap.registerPlugin(Draggable);
+const WindowWrapper = (Component, windowKey) => {
+    const Wrapped  = (props) => {
+        const {focusWindow, windows} = useWindowStore();
+        const { isOpen, isMinimized, isMaximized, zIndex } = windows[windowKey];
+
+        const ref = React.useRef(null);
+
+        useGSAP(() => {
+            if (!ref.current || !isOpen) return () => {};
+            const el = ref.current;
+            el.style.display = "block";
+
+            gsap.fromTo(el,
+                { scale: 0.8, opacity: 0, y:40},
+                { scale: 1, opacity: 1, y:0, duration: 0.5, ease: "power2.out" }
+            );
+
+
+        }, [isOpen]);
+
+        useGSAP(() => {
+            const el = ref.current;
+            if (!el) return () => {};
+            if (isMaximized) {
+    gsap.set(el, { x: 0, y: 0 });
+            }
+            const [instance] =  Draggable.create(el, {onPress: () => focusWindow(windowKey)});
+            return () => {
+                instance.kill();
+            };
+        },[isMaximized]);
+
+        useLayoutEffect(() => {
+            const el = ref.current;
+            if (!el) return () => {};
+            el.style.display = isOpen ? 'block' : 'none';
+        }, [isOpen]);
+
+       return (
+        <section
+          id={windowKey}
+          ref={ref}
+          style={{ zIndex }}
+          className={`absolute ${isMaximized ? 'window-maximized' : ''}`}
+        >
+            <Component {...props} />
+        </section>
+);
+
+    }
+
+    Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || 'Component'})`;
+  return Wrapped;
+}
+
+export default WindowWrapper
